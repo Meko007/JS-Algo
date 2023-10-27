0f77ca99801677d4648c79b8674ebbd9598a28fe
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-10-27 20:41:19 +0100

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,6 @@
 const palindromeChecker = (str) => {
   str = str.toLowerCase();
-  const regex = /\W/g;
+  const regex = /\W/g; //a regular expression to match non alphamumeric characters (whitespace, periods etc)
 
   const cleanedString = str.replace(regex, '');
   let reversed = '';
@@ -8,10 +8,10 @@ const palindromeChecker = (str) => {
   for(let i = cleanedString.length - 1; i >= 0; i--){
     reversed += cleanedString[i];
   }
-
-  if(reversed === cleanedString){
-    console.log('A palindrome');
-  }else{
-    console.log('Not a palindrome');
-  }
+ return reversed === cleanedString ? "A palindrome" : "Not a palindrome";
 }
+
+//Test cases
+//console.log(palindromeChecker('My age is 0, 0 si ega ym.'));
+//console.log(palindromeChecker('101'));
+//console.log(palindromeChecker('Civic'));
