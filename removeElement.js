d0eb231c31f87be2d6d79f083ea11ba2de26b341
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-10-28 21:47:36 +0100

    Create removeElement.js

---
+++ removeElement.js
@@ -0,0 +1,23 @@
+var removeElement = (nums, val) =>{
+  nums = nums.filter(item => item !== val);
+  return nums;
+};
+
+//Test cases
+//console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+//console.log(removeElement([3, 2, 2, 3], 3));
+
+
+//The output of the code below is the length of the array after removing the element(s)
+/*
+var removeElement = function(nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+};
+*/
