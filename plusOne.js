50b0d804e8efb8f9d081d31fea9d6a29cb6a8e2a
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-10-28 21:09:47 +0100

    Update plusOne.js

--- plusOne.js
+++ plusOne.js
@@ -1,5 +1,5 @@
 var plusOne = function(digits) {
-  let num = BigInt(digits.join(''));
+  let num = BigInt(digits.join('')); //converting the array into a BigInt data type
   num += BigInt(1);
-  return String(num).split('').map(BigInt);
+  return String(num).split('').map(BigInt); //converts the number back to an array
 };
