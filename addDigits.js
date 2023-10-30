0dd48c77deffc64a2ce3b36083b81769246a9315
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-10-30 22:36:19 +0100

    Create addDigits.js

---
+++ addDigits.js
@@ -0,0 +1,10 @@
+//Add the digits of a number until a single digit remains
+
+const addDigits = function(num){
+  return 1 + (num - 1) % 9; /*using a mathematical approach; every number that goes through modulo 9 will return a single digit between 0-9;
+  why modulo 9?, well 9 is the last single digit number before 10*/
+};
+
+//Test cases
+//console.log(addDigits(3899));
+//console.log(addDigits(645));
