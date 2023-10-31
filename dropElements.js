ffeb2ff4a95052488e7f1cbd855e3aaae2c94327
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-10-31 19:12:18 +0100

    Create dropElements.js

---
+++ dropElements.js
@@ -0,0 +1,15 @@
+/*
+Given the array arr, this function iterates through and removes each element starting from the first element (the 0 index) 
+until the function "func" returns true when the iterated element is passed through it.
+*/
+const dropElements = (arr, func) =>{
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
+  return arr;
+};
+
+//Test cases
+//console.log(dropElements([1, 2, 3, 4], function(n) { return n >= 3;}));
+//console.log(dropElements([0, 1, 0, 1], function(n) { return n === 1;}));
+//console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
