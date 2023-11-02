59c1730d9da974d0937dc84707f871c2817a90c9
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-11-02 22:04:53 +0100

    Create janken.js

---
+++ janken.js
@@ -0,0 +1,22 @@
+//jan ken pon => rock paper scissors
+const janken = (choice) =>{
+  choice = choice.toLowerCase();
+  let compChoice = (Math.floor(Math.random() * 3)) + 1;  //1 for rock, 2 for paper, 3 for scissors
+
+  if((choice === "rock" && compChoice === 3) ||
+    (choice === "paper" && compChoice === 1) ||
+    (choice === "scissors" && compChoice === 2)) return "You win";
+  else if((choice === "rock" && compChoice === 1) ||
+    (choice === "paper" && compChoice === 2) ||
+    (choice === "scissors" && compChoice === 3)) return "A tie";
+  else if((choice === "rock" && compChoice === 2) ||
+    (choice === "paper" && compChoice === 3) ||
+    (choice === "scissors" && compChoice === 1)) return "You lose";
+  else return "You can only input rock, paper or scissors";
+}
+
+//Test cases
+// console.log(janken("rock"));
+// console.log(janken("paper"));
+// console.log(janken("scissors"));
+
