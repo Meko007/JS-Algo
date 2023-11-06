ff3dfc65af03461bf9b020f08f9ed9cb294b9e0d
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-11-06 14:18:06 +0100

    Update containAllRots.js

--- containAllRots.js
+++ containAllRots.js
@@ -1,13 +1,18 @@
-//Checks if an array contains all variations of a string
+//Checks if an array contains all rotations of a string
 const containAllRots = (strng, arr) =>{
-  let count = 0;
-  let regex = /[strng]/gi;
-  for(let i = 0; i < arr.length; i++){
-    if(regex.test(arr[i])) count++;
-  }
-  return count === strng.length ? true : false;
+    if (strng === '') return true;
+
+    for (let i = 0; i < strng.length; i++) {
+        //checking if the index of a rotated string is === -1
+        if(arr.indexOf(strng.slice(i) + strng.slice(0, i)) === -1) return false;
+    }
+
+    return true;
 };
 
 //Test cases
-//console.log(containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]));
-//console.log(containAllRots("XjYABhR", ["TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY"]));
+// console.log(containAllRots("bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]));
+// console.log(containAllRots("", ["bsjq", "qbsj"]));
+// console.log(containAllRots("XjYABhR", ["TzYxlgfnhf", "yqVAuoLjMLy", "BhRXjYA", "YABhRXj", "hRXjYAB", "jYABhRX", "XjYABhR", "ABhRXjY"]));
+// console.log(containAllRots("Okemdinachi", ["kemdinachiO", "emdinachiOk", "mdinachiOke", "dinachiOkem", "inachiOkemd", "nachiOkemdi", "achiOkemdin", "chiOkemdina", "hiOkemdinac", "iOkemdinach", "Okemdinachi"]));
+
