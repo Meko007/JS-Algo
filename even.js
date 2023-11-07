3ea1e11a440742fbef7fc0cf49814b070e9ab2ee
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-11-07 22:08:38 +0100

    Create even.js

---
+++ even.js
@@ -0,0 +1,10 @@
+//Checking if a number is even without using the traditional '%'
+const isEven = n => { return Number.isInteger(n / 2) ? true : false; };
+
+//Test cases
+// console.log(isEven(2));
+// console.log(isEven(3));
+// console.log(isEven(14));
+// console.log(isEven(15));
+// console.log(isEven(26));
+// console.log(isEven(27));
