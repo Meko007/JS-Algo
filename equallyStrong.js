b53b4e718f90a6be9b99138bb38bc5344e780425
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-11-08 19:04:35 +0100

    Create equallyStrong.js

---
+++ equallyStrong.js
@@ -0,0 +1,22 @@
+/*
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+*/
+const areEquallyStrong = (yourLeft, yourRight, friendsLeft, friendsRight) => {
+    if(yourLeft < 0 || yourRight < 0 || friendsLeft < 0 || friendsRight < 0) return false;
+    return ((yourLeft === friendsLeft && yourRight === friendsRight) || 
+    (yourRight === friendsLeft && yourLeft === friendsRight)) ? true : false;
+};
+
+//Test cases
+// console.log(areEquallyStrong(10, 15, 15, 10));
+// console.log(areEquallyStrong(15, 10, 15, 10));
+// console.log(areEquallyStrong(10, 10, 10, 10));
+// console.log(areEquallyStrong(15, 10, 15, 9));
+// console.log(areEquallyStrong(10, 5, 5, 10 ));
+// console.log(areEquallyStrong(1, 10, 10,0));
+// console.log(areEquallyStrong(10, 5, 11, 4));
+// console.log(areEquallyStrong(-1, 10, 4, 5));
