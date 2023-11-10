3c36e86162fd5818092f87ae02abd94f9e5c3a69
Okem Onyeka <69441911+Meko007@users.noreply.github.com>
2023-11-10 20:47:04 +0100

    Create chunk.js

---
+++ chunk.js
@@ -0,0 +1,20 @@
+const chunkArrayInGroups = (arr, size) => {
+    let temp = [];
+    const result = [];
+
+    for (let a = 0; a < arr.length; a++) {
+        if (a % size !== size - 1) temp.push(arr[a]);
+        else {
+            temp.push(arr[a]);
+            result.push(temp);
+            temp = [];
+        }
+    }
+
+    if (temp.length !== 0) result.push(temp);
+    return result;
+};
+
+//Test cases
+// console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+// console.log(chunkArrayInGroups(["a", "b", "c", "d", 1, 2, 3, 4], 3));
