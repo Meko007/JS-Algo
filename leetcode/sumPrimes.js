5d06f10e498be8b48f8d450c4285215cfd29859b
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-14 20:40:05 +0100

    leetcode and codewars folders

--- leetcode/sumPrimes.js
+++ leetcode/sumPrimes.js
@@ -11,15 +11,13 @@ const sumPrimes = range =>{
           }
       }
    //the for loop starts dividing from 2 and stops just before the number that's being checked
-      if(isPrime){
-          sum += number;
-      }
+      if(isPrime) sum += number;
 
       number++;
   }while(number <= range){
     return sum;
   }
-}
+};
 
 //Test cases
 //console.log(sumPrimes(100000));
---
+++ leetcode/twoSum.js
@@ -0,0 +1,16 @@
+//returns the array indexes that add up to the target
+const twoSum = (nums, target) =>{
+    let map = new Map();
+
+    for(let i = 0; i < nums.length; i++){
+        let diff = target - nums[i];
+
+        if(map.has(diff)) return [map.get(diff), i];
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+//Test cases
+//console.log(twoSum([1, 4, 5, 6, 11], 17));
+//console.log(twoSum([3,3], 6));
