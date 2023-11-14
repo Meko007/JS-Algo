dee730fd53b5dd8491172c69f0f670bac753c67e
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-14 01:17:31 +0100

    Update alphanumeric.js

--- alphanumeric.js
+++ alphanumeric.js
@@ -5,7 +5,7 @@ The string has the following conditions to be alphanumeric:
 2. Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
 3. No whitespaces / underscore
 */
-const alphanumeric = str => { return (/^[a-zA-Z0-9]+$/gi).test(str); }
+const alphanumeric = str => { return (/^[a-zA-Z0-9]+$/gi).test(str); };
 
 //Test cases
 // console.log(alphanumeric("Mazinkaiser"));
