a1902bf3f215a7f04b54428d4cf8ced2882871d3
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-17 19:59:50 +0100

    Update sumStrings.js

--- codewars/4kyu/sumStrings.js
+++ codewars/4kyu/sumStrings.js
@@ -8,6 +8,14 @@ A string representation of an integer will contain no characters besides the ten
 */
 const sumStrings = (a, b) => { return (BigInt(a) + BigInt(b)).toString(); };
 
+// OR
+
+// const sumStrings = (a, b) => {
+//     let num1 = BigInt(a);
+//     let num2 = BigInt(b);
+//     return (num1 + num2).toString();
+// }
+
 //Test cases
 // console.log(sumStrings('123', '456'));
 // console.log(sumStrings('134223', '2321212'));
