c1cc16d4d76a359b7153aec4a423f83c93568b29
Okem Onyeka <jesse007onyeka@gmail.com>
2023-11-17 19:49:43 +0100

    Create /4kyu/sumStrings.js

---
+++ codewars/4kyu/sumStrings.js
@@ -0,0 +1,14 @@
+/*
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+sumStrings('1','2') // => '3'
+
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+*/
+const sumStrings = (a, b) => { return (BigInt(a) + BigInt(b)).toString(); };
+
+//Test cases
+// console.log(sumStrings('123', '456'));
+// console.log(sumStrings('134223', '2321212'));
+// console.log(sumStrings('123112', '2232323'));
