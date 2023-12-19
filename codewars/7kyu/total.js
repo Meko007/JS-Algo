ae51cf59eba35cfd34e3878e379a414929cf4e35
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-19 23:50:38 +0100

    Create total.js

---
+++ codewars/7kyu/total.js
@@ -0,0 +1,17 @@
+/*
+Create a function that returns the total of a meal including tip and tax. 
+You should not tip on the tax.
+
+You will be given the subtotal, the tax as a percentage and the tip as a percentage. 
+Please round your result to two decimal places.
+*/
+const calculate_total = (subtotal, tax, tip) => 
+    parseFloat(((subtotal * (tax / 100)) + (subtotal * (tip / 100)) + subtotal).toFixed(2));
+
+
+//Test cases
+// console.log(calculate_total(5, 5, 10));
+// console.log(calculate_total(36.97, 7, 15));
+// console.log(calculate_total(0.00, 6, 18));
+// console.log(calculate_total(80.94, 0, 20));
+// console.log(calculate_total(54.96, 8, 0));
