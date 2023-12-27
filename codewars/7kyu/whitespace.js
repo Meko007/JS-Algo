edb0ba8e86998191a47ac8a12b7973719d9e820b
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-27 14:50:26 +0100

    some manual linting

--- codewars/7kyu/whitespace.js
+++ codewars/7kyu/whitespace.js
@@ -3,9 +3,9 @@ Implement String#whitespace?(str) (Ruby), String.prototype.whitespace(str) (Java
 String::whitespace(str) (CoffeeScript), or whitespace(str) (Python), 
 which should return true/True if given object consists exclusively of zero or more whitespace characters, false/False otherwise.
 */
-String.prototype.whitespace=function(){
+String.prototype.whitespace = function () {
     return /^\s*$/.test(this);
-}
+};
 
 //Test cases
 // console.log(''.whitespace());
