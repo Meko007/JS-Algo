30a0cbffabdefe485f2ad9e54539a29a6845bfb5
Okem Onyeka <jesse007onyeka@gmail.com>
2023-12-30 21:22:52 +0100

    Create opposite.(js/ts)

---
+++ codewars/8kyu/opposite.js
@@ -0,0 +1,18 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+const opposite = number => 0 - number;
+
+// Test cases
+// console.log(opposite(1));
+// console.log(opposite(0));
+// console.log(opposite(-5));
+// console.log(opposite(-12525220.3325));
+// console.log(opposite(3.3333333));
+// console.log(opposite(4.25));
