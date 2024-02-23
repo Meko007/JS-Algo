39bded329ff32b3d370b7694a18c42b216ecf7bb
Okem Onyeka <jesse007onyeka@gmail.com>
2024-02-23 20:23:47 +0100

    reduce.(js/ts)

---
+++ codewars/8kyu/reduce.ts
@@ -0,0 +1,11 @@
+const reduce = (arr: number[]) => {
+    let num = 0;
+    for (let i = arr[0]; i >= 1; i--) {
+        if (arr[0] % i === 0 && arr[1] % i === 0) {
+            num = i;
+        } 
+    }
+    return [(arr[0] / num), (arr[1] / num)];
+};
+
+console.log(reduce([45, 120]));
